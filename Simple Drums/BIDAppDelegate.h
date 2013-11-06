c8095b2f5467cbda2822bbbc90e6ec42f78a0868
vberthiaume <nicolai@nicolai.sf.umontreal.ca>
2013-11-06 13:31:46 -0500

    updated license info and added .gitignore

--- Simple Drums/BIDAppDelegate.h
+++ Simple Drums/BIDAppDelegate.h
@@ -2,9 +2,22 @@
 //  BIDAppDelegate.h
 //  Simple Drums
 //
-//  Created by Nicolai on 2013-09-24.
-//  Copyright (c) 2013 Bonzai Studios. All rights reserved.
-//
+//  Copyright 2013 Vincent Berthiaume
+// 
+//  This file is part of Simple Drums.
+// 
+//  Simple Drums is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  Simple Drums is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with Simple Drums.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <UIKit/UIKit.h>
 
